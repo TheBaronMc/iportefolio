e2f6ba6b1b4b83920935c4df67bd9f93583092a0
TheBaronMc <baron_mc@yahoo.com>
2021-12-20 20:10:48 +0100

    CV_Skill & CV_Projects

---
+++ frontend/src/components/CV_Components/CV_Project.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="block">
+        <h2>Projects</h2>
+        <div>
+            <div v-for="project in projects">
+                <div class="container">
+                    <div class="project_name">
+                        <h3>{{project.name}}</h3>
+                        <h4 class="date">{{project.beginDate}} -> {{project.endDate}}</h4>
+                    </div>
+                    <div class="project_description">
+                        <p>{{project.description}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'CVProjects',
+    data() {
+        return {projects: []}
+    },
+    mounted() {
+        fetch(`http://localhost:3000/api/cv/project`, { method: 'GET' })
+        .then(res => res.json())
+        .then((value) => {
+            this.projects = value
+        })
+        .catch(error => console.log(error.message));
+    }
+}
+</script>
+
+<style scoped>
+
+.block {
+    background-color: white;
+    padding-left: 2em;
+    padding-right: 2em;
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: flex-start;
+}
+
+.project_name {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.project_description {
+    padding-left: 1em;
+    padding-right: 1em;
+}
+
+.date {
+    color: grey;
+}
+
+
+</style>
---
+++ frontend/src/components/CV_Components/CV_Skill.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="block">
+        <h2>Skills</h2>
+        <div class="container">
+            <p v-for="skill in skills" class="skill">
+                {{skill.skill}}
+            </p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'CVSkill',
+    data() {
+        return {skills: []}
+    },
+    mounted() {
+        fetch(`http://localhost:3000/api/cv/skill`, { method: 'GET' })
+        .then(res => res.json())
+        .then((value) => {
+            this.skills = value
+        })
+        .catch(error => console.log(error.message));
+    }
+}
+</script>
+
+<style scoped>
+
+.block {
+    background-color: white;
+    padding-bottom: 1em;
+}
+
+.container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-evenly;
+}
+
+.skill {
+    border: solid 1px;
+    border-radius: 5px;
+    background: #22c0e8;
+    padding: 10px;
+    box-shadow: 1px 1px gray;
+}
+
+</style>
