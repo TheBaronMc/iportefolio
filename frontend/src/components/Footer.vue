f5dd8356e1176a5c91847f84c6d67e6cf846ca3a
charly-ginevra <charly-ginevra@gitlab.com>
2021-12-31 10:34:07 +0100

    add links to footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,24 +1,51 @@
 <template>
     <div class="footer">
         <p>iportfolio by Charly Ginevra</p>
+        <div>
+            <a v-for="media in ['facebook', 'instagram', 'github', 'linkedin']" v-bind:href="links[media]" target="_blank">
+                <img :src="require(`@/assets/${media}_white.svg`)" alt="">
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
+import { fecthContact } from '@/service/fetchData'
+
 export default {
   name: 'Footer',
   setup() {
-
-  }
+      return {links: {facebook: '', instagram: '', github: '', linkedin: ''}}
+  },
+  async mounted() {
+      let res = await fecthContact()
+      res = res[0]
+      for (let l of ['facebook', 'instagram', 'github', 'linkedin']) {
+          this.links[l] = res[l].link
+      } 
+      console.log(this.links);
+  },
 }
 </script>
 
 <style scoped>
+img {
+    width: 20px;
+    height: 20px;
+}
+
+p {
+    margin: 0;
+}
+
 .footer {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     background-color: #333;
     color: white;
+    padding-top: 15px;
+    padding-bottom: 5px;
 }
 </style>
