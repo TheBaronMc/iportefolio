20a6b9719ffef7b4c79d06c63fc83c9933be61b3
charly-ginevra <charly-ginevra@gitlab.com>
2022-01-01 11:17:32 +0100

    not found page

---
+++ frontend/src/components/Code404.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="block">
+        <h1>404 Not Found</h1>
+        <p>This page doesn't exist.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Code404'
+}
+</script>
+
+<style scoped>
+.block {
+    background-color: white;
+    padding: 2em;
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <Code404></Code404>
+    </div>
+</template>
+
+<script>
+import Code404 from '@/components/Code404.vue'
+
+export default {
+    name: 'NotFound',
+    components: {
+        Code404
+    }
+}
+</script>
+
+<style scoped>
+
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
